c24c683bc1279bf6143636cdcf82668b45953ccb
Toto <jean@naio-technologies.com>
2016-11-29 14:52:54 +0100

    fixed cp -r bug => work1/2/3 random problem

--- naio_tc_agent/docker-entrypoint.sh
+++ naio_tc_agent/docker-entrypoint.sh
@@ -7,12 +7,15 @@ fi
 
 echo "Starting buildagent..."
 
-cp -r /opt/buildagent/conf_dist/ /data/teamcity_agent/conf
+cp -r /opt/buildagent/conf_dist/* /data/teamcity_agent/conf
 cp /data/teamcity_agent/conf/buildAgent.dist.properties /data/teamcity_agent/conf/buildAgent.properties
 sed -i "s#^workDir=.*\$#workDir=../work${TEAMCITY_AGENT_NUMBER}#" /data/teamcity_agent/conf/buildAgent.properties
 sed -i "s#^tempDir=.*\$#tempDir=../temp${TEAMCITY_AGENT_NUMBER}#" /data/teamcity_agent/conf/buildAgent.properties
 sed -i "s#^name=.*\$#name=agent_${TEAMCITY_AGENT_NUMBER}#" /data/teamcity_agent/conf/buildAgent.properties
 sed -i "s#^serverUrl=.*\$#serverUrl=${SERVER_URL}#" /data/teamcity_agent/conf/buildAgent.properties
+
+sync
+sleep 3
 
 /run-agent.sh >& /var/log/run_agent.log
 
